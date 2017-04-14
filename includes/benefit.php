ac1d4cc59f0997e8e273985c67f2409ae658464d
engine77 <zaur.1kager@gmail.com>
2017-04-14 17:51:04 +0400

    inputtext

---
+++ includes/benefit.php
@@ -0,0 +1,50 @@
+<div class="section section_padding">
+	<div class="dfbenefit dfbenefit_theme_rose">
+
+		<div class="dfbenefit__leftblock dfbenefit__leftblock_theme_rose">
+			<h2 class="h2 h2_theme_rosa h2_white dfbenefit__caption">Кому это будет выгодно?</h2><div class="clearfix"></div>
+			<span class="dfbenefit__subtitle">По словам представителей Налоговой  службы, нововведение позволит:</span>	
+			<div class="clearfix"></div>			
+		</div>
+
+		<div class="dfbenefit__rightblock dfbenefit__rightblock_theme_rose">
+			<div class="dfbenefit__txtbox">
+				<div class="dfbenefit__txtbox__img">
+					<img src="img/wallet.png" alt="">
+				</div>
+				<p class="dfbenefit__txtbox__text">Cнизить ежегодные расходы <br> на ККТ</p>
+			</div>	
+			<div class="dfbenefit__txtbox">
+				<div class="dfbenefit__txtbox__img">
+					<img src="img/tech.png" alt="">
+				</div>
+				<p class="dfbenefit__txtbox__text">Применять в составе ККТ современные электронные устройства – мобильные телефоны и планшеты</p>
+			</div>	
+			<div class="dfbenefit__txtbox">
+				<div class="dfbenefit__txtbox__img">
+					<img src="img/list.png" alt="">
+				</div>
+				<p class="dfbenefit__txtbox__text">Иметь возможность через сайт ФНС России зарегистрировать ККТ без ее физического предоставления в налоговый орган</p>
+			</div>	
+			<div class="dfbenefit__txtbox">
+				<div class="dfbenefit__txtbox__img">
+					<img src="img/search.png" alt="" >
+				</div>
+				<p class="dfbenefit__txtbox__text">Избавиться от проверок, так как оперативное получение информации о расчетах обеспечивает соответствующую среду доверия</p>
+			</div>	
+			<div class="dfbenefit__txtbox">	
+				<div class="dfbenefit__txtbox__img">
+					<img src="img/graph.png" alt="">			
+				</div>
+				<p class="dfbenefit__txtbox__text">Получить инструмент, с помощью которого предприниматель сможет в режиме реального времени следить за своими оборотами, показателями, и лучше контролировать свой бизнес</p>
+			</div>	
+			<div class="dfbenefit__txtbox">
+				<div class="dfbenefit__txtbox__img">
+					<img src="img/balance.png" alt="">
+				</div>
+				<p class="dfbenefit__txtbox__text">Работать в условиях честной конкурентной бизнес среды за счет пресечения возможности недобросовестных налогоплательщиков незаконно минимизировать свои налоговые обязательства и нечестно получать конкурентное преимущество</p>
+			</div>	
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</div>			
--- includes/header.php
+++ includes/header.php
@@ -17,6 +17,7 @@
 	<link rel="stylesheet" href="css/fonts.css">
 	<link rel="stylesheet" href="css/style.css">
 	<link rel="stylesheet" href="block/button/button.css">
+	<link rel="stylesheet" href="block/h2/h2.css">
 	<link rel="stylesheet" href="block/inputtext/style.css">
 	<link rel="stylesheet" href="block/q/q.css">
 	<link rel="stylesheet" href="css/style-db.css">
