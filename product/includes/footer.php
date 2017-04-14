8f82b99fa462333f301115988ae980f95d7519d5
kvsemenoff <kvsemenoff@mail.ru>
2017-04-14 17:47:03 +0400

    d

--- product/includes/footer.php
+++ product/includes/footer.php
@@ -1,12 +1,3 @@
-
-<script>
-	$(document).ready(function() {
-		$("head").append("<link href='https://fonts.googleapis.com/css?family=Fira+Sans+Condensed:300i,500,600,700&amp;subset=cyrillic' rel='stylesheet'>");
-		$("head").append("<link href='//netdna.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.css' rel='stylesheet'>");
-
-	});
-</script>
-
 <script src="libs/jquery/jquery-1.11.1.min.js"></script>
 <script src="libs/owl.carousel/owl.carousel.js"></script>
 <script src="libs/fancybox/jquery.fancybox.pack.js"></script>
--- product/includes/header.php
+++ product/includes/header.php
@@ -4,7 +4,12 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Главная</title>
-	
+	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
+	<link href="//netdna.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.css" rel="stylesheet">
+	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
+	<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />
+	<link type="text/css" href="css/jquery.jscrollpane.css" rel="stylesheet" media="all" />
+	<link href="https://fonts.googleapis.com/css?family=Fira+Sans+Condensed:300i,500,600,700&amp;subset=cyrillic" rel="stylesheet">
 	<link rel="shortcut icon" href="favicon.ico" />
 
 
@@ -24,13 +29,14 @@
 	<link rel="stylesheet" href="css/style-di.css">
 	<link rel="stylesheet" href="css/style-dj.css">
 
-	<?php require_once('includes/header-top.php'); ?>
-
-	<?php require_once('includes/changes.php'); ?>
-	<?php require_once('includes/reality.php'); ?>
-	<?php require_once('includes/benefit.php'); ?>
+	
 
 	
 
 </head>
 <body>
+	<?php require_once('includes/header-top.php'); ?>
+
+	<?php require_once('includes/changes.php'); ?>
+	<?php require_once('includes/reality.php'); ?>
+	<?php require_once('includes/benefit.php'); ?>
