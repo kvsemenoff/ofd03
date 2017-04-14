973cf10626088ea3042af421b55a0f1d953844d7
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-04-14 21:22:39 +0600

    added autonomous

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 <?php require_once('includes/changes.php'); ?>
 <?php require_once('includes/reality.php'); ?>
 <?php require_once('includes/benefit.php'); ?>
+<?php require_once('includes/autonomous.php'); ?>
+<?php require_once('includes/autonomous-next.php'); ?>
 
 <?php require_once('includes/footer.php'); ?>
 </body>
